bfe4aea43424f04b42f165f62a4c382e00366a51
Gustavo Vinicius <gus.tec.dev@gmail.com>
2024-11-11 19:04:22 -0300

    [lista_coisas - feature] salvando a lista no dispositivo do usuario

--- lista_coisas/lib/screens/FormList.dart
+++ lista_coisas/lib/screens/FormList.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:lista_coisas/components/InputText.dart';
 import 'package:lista_coisas/components/ButtonSubmit.dart';
 import 'package:lista_coisas/components/ListThings.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Formlist extends StatefulWidget {
   Formlist({super.key});
@@ -18,6 +19,7 @@ class _FormlistState extends State<Formlist> {
   @override
   void initState() {
     super.initState();
+    _loadItems();
     _textController.addListener(() {
       if (errorMessage != null) {
         setState(() {
@@ -31,12 +33,28 @@ class _FormlistState extends State<Formlist> {
     setState(() {
       if (_textController.text.isNotEmpty) {
         items.add(_textController.text);
+        _saveItems();
         _textController.clear();
         errorMessage = null;
       } else {
         errorMessage = 'Preencha esse campo.';
       }
     });
+  }
+
+  // Carregar a lista salva na memória
+  Future<void> _loadItems() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      items = prefs.getStringList('items') ??
+          []; // Carregar a lista salva, ou uma lista vazia
+    });
+  }
+
+  // Salvar a lista no shared preferences
+  Future<void> _saveItems() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setStringList('items', items); // Salvar a lista no dispositivo
   }
 
   @override
